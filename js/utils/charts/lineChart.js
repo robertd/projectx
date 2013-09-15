7eb3694ee7d8be0db835a921c2420966ee9a9bff
robertd <robert.djurasaj@gmail.com>
2013-09-14 22:43:38 -0600

    Cleanup line chart

--- js/utils/charts/lineChart.js
+++ js/utils/charts/lineChart.js
@@ -48,46 +48,6 @@ define([
             nv.utils.windowResize(chart.update);
             return chart;
         });
-
-        function sinAndCos() {
-  var sin = [],
-    cos = [],
-    rand = [],
-    rand2 = []
-    ;
-
-  for (var i = 0; i < 100; i++) {
-    sin.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) }); //the nulls are to show how defined works
-    cos.push({x: i, y: .5 * Math.cos(i/10)});
-    rand.push({x:i, y: Math.random() / 10});
-    rand2.push({x: i, y: Math.cos(i/10) + Math.random() / 10 })
-  }
-
-  return [
-    {
-      area: true,
-      values: sin,
-      key: "Sine Wave",
-      color: "#ff7f0e"
-    },
-    {
-      values: cos,
-      key: "Cosine Wave",
-      color: "#2ca02c"
-    },
-    {
-      values: rand,
-      key: "Random Points",
-      color: "#2222ff"
-    }
-    ,
-    {
-      values: rand2,
-      key: "Random Cosine",
-      color: "#667711"
-    }
-  ];
-}
     };
     return LineChart;
 });
