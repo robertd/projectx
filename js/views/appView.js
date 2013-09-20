33edb4288c28d91ba91b6df4409017527903e880
Robert Djurasaj <robert.djurasaj@gmail.com>
2013-09-20 14:47:34 -0600

    Invoke accounts view

--- js/views/appView.js
+++ js/views/appView.js
@@ -3,8 +3,9 @@ define([
     "views/accountsLineView",
     "views/accountsStackedBarView",
     "views/investmentsView",
-    "views/assetsView"
-], function (Backbone, AccountsLineView, AccountsStackedBarView, InvestmentsView, AssetsView) {
+    "views/assetsView",
+    "views/accountsStatusView"
+], function (Backbone, AccountsLineView, AccountsStackedBarView, InvestmentsView, AssetsView, AccountsStatusView) {
 	"use strict";
 	
     var AppView = Backbone.View.extend({
@@ -13,6 +14,7 @@ define([
             this.accountsStackedBarView = new AccountsStackedBarView();
             this.investmentsView = new InvestmentsView();
             this.assetsView = new AssetsView();
+            this.accountsStatusView = new AccountsStatusView();
         }
     });
     return AppView;
